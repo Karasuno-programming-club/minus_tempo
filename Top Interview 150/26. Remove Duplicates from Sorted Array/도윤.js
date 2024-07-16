82c219e4c421762202dd18ecbefa042c6086816d
지성김 <jiseong-gim@jiseong-gim-ui-MacBookPro.local>
2024-07-17 08:48:56 +0900

    fix: 0717 도윤

--- Top Interview 150/26. Remove Duplicates from Sorted Array/도윤.js
+++ Top Interview 150/26. Remove Duplicates from Sorted Array/도윤.js
@@ -4,14 +4,16 @@
  */
 var removeDuplicates = function(nums) {
   
-    let i = 0;
+    let p = 0;
 
-    for (let j = 1; j < nums.length; j++) {
-        if (nums[j] !== nums[i]) {
-            i++;
-            nums[i] = nums[j];
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[p]) {
+            p++;
+            nums[p] = nums[i];
         }
     }
 
     return i + 1;
 }; 
+
+removeDuplicates([0,0,1,1,1,2,2,3,3,4]); // 5
