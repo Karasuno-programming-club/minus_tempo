12e1a8cbe7ac64ddb165e2eb7d5be97d50df75ee
지성김 <jiseong-gim@jiseong-gim-ui-MacBookPro.local>
2024-07-18 07:08:40 +0900

    fix: 리버스 방법 풀이

--- Top Interview 150/189. Rotate Array/도윤.js
+++ Top Interview 150/189. Rotate Array/도윤.js
@@ -27,3 +27,33 @@ rotate([1,2,3,4,5,6,7],3);
 /**
  * 시간 복잡도 O(N)
  */
+
+/**
+ * reverse 방법
+ */
+
+var rotate2 = function(nums, k) {
+
+    k = k % nums.length;
+
+    const reverse = (nums, start, end) => {
+        while(start < end) {
+            let tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            start++;
+            end--;
+        }  
+    };
+
+    reverse(nums, 0, nums.length - 1); // [7,6,5,4,3,2,1] 전체 reverse
+    reverse(nums, 0, k - 1); // [5,6,7,4,3,2,1] 로테이션 숫자만 reverse
+    reverse(nums, k, nums.length - 1); // [5,6,7,1,2,3,4] 나머지 숫자만 reverse  
+};
+
+rotate2([1,2,3,4,5,6,7],3);
+
+
+/**
+ * 시간 복잡도 O(n)
+ */
