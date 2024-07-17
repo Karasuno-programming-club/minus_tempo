627d426f9301e414887a5ec7aacd490752365b95
root-zero-o <rootzero17@gmail.com>
2024-07-17 21:25:08 +0900

    240717 근영 - 결국 해답봄 ㅠㅠ

--- Top Interview 150/189. Rotate Array/근영.js
+++ Top Interview 150/189. Rotate Array/근영.js
@@ -1,6 +1,7 @@
 // 2024.07.17.
 // https://leetcode.com/problems/rotate-array/?envType=study-plan-v2&envId=top-interview-150
 
+// 풀다보니 space complexity가 O(N)
 var rotate = function (nums, k) {
   let length = nums.length;
   let rotateNum = nums.length - (k % nums.length);
@@ -12,4 +13,23 @@ var rotate = function (nums, k) {
   for (let i = 0; i < rotateNum; i++) {
     nums.shift();
   }
+};
+
+// solutions
+var rotate = function(nums, k) {
+  k = k % nums.length; // k번만큼 로테이션 돌기 때문에 길이만큼 돌아도 결국 같음 -> 나머지만큼만 돌면됨
+
+  const reverse = (i, j) => {
+      while(i < j){
+          let temp = nums[i];
+          nums[i] = nums[j];
+          nums[j] = temp;
+          i++;
+          j--;
+      }
+  }
+
+  reverse(0, nums.length - 1);
+  reverse(0, k-1);
+  reverse(k, nums.length - 1);
 };
