825c1a3ee2952b1511839b1a1d00240679eaa19b
Lifthus <lifthus531@gmail.com>
2024-07-18 18:39:12 +0900

    오늘의 추천곡: Wonderland - TS

--- Top Interview 150/20240718 122. Best Time to Buy and Sell Stock II/정훈.py
+++ Top Interview 150/20240718 122. Best Time to Buy and Sell Stock II/정훈.py
@@ -1,1 +1,17 @@
-
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # 팔거나 사거나, 안사거나
+        bought = False
+        profit = 0
+        for i in range(len(prices) - 1):
+            if not bought:
+                if prices[i+1] > prices[i]:
+                    bought = True
+                    bPrice = prices[i]
+            else:
+                if prices[i+1] <= prices[i]:
+                    bought = False
+                    profit += prices[i]-bPrice
+        if bought:
+            profit += prices[-1]-bPrice
+        return profit
