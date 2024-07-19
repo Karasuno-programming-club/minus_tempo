46d99abe0c42306210d8cd4ef94ccbd48f5a402f
sorryisme <wivipp39@gmail.com>
2024-07-19 22:03:48 +0900

    fix: 풀긴 풀었으나 내 실력으로 풀지 않음 칚쑈

---
+++ Top Interview 150/[240719] 55. Jump Game/도윤.js
@@ -0,0 +1,26 @@
+var canJump = function(nums) {
+
+
+    let result = false;
+    let jumpIdx = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+
+        if( i > jumpIdx ) {
+            return false;
+        }
+
+        jumpIdx = Math.max(jumpIdx, i + nums[i]);
+
+        if (jumpIdx >= nums.length - 1) { 
+            result = true;
+        }
+
+       
+    }
+
+    return result    
+};
+
+canJump([3,2,1,0,4]);
+ 
