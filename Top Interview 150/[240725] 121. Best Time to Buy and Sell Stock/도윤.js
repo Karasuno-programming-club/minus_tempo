4c28d4d9855bb1c8427a9863f86cad70c2e312b1
지성김 <jiseong-gim@jiseong-gim-ui-MacBookPro.local>
2024-07-25 08:17:22 +0900

    fix: 0725 도윤

---
+++ Top Interview 150/[240725] 121. Best Time to Buy and Sell Stock/도윤.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let minPrice = prices[0];
+    let profit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        if (minPrice > prices[i]) {
+            minPrice = prices[i];
+            continue;
+        }
+
+        if (profit < prices[i] - minPrice) {
+            profit = prices[i] - minPrice;
+        }
+    }
+
+    return profit
+};
+
+maxProfit([7,1,5,3,6,4]);
