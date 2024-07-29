625e87da5aa443734a1a201c451ca684f538878b
root-zero-o <rootzero17@gmail.com>
2024-07-30 07:08:29 +0900

    240730

---
+++ Top Interview 150/[240730] 125. Valid Palindrome/근영.js
@@ -0,0 +1,17 @@
+
+var isPalindrome = function(s) {
+    let forward = '';
+    let backward = '';
+    let lowerS = s.toLowerCase()
+
+    for(let i = 0; i < s.length; i++){
+        let charCode = lowerS.charCodeAt(i);
+        if((charCode >= 97 && charCode <= 122) || (charCode >= 48 && charCode <= 57)){
+            forward += lowerS[i]
+            backward = lowerS[i] + backward
+        }
+    }
+    return forward === backward
+};
+
+//  Alphanumeric : 숫자도 포함된다
