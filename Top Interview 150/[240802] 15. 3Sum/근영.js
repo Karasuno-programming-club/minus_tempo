e8d41cb5f3071a494d69d21c680df511d905f713
root-zero-o <rootzero17@gmail.com>
2024-08-03 21:49:41 +0900

    240803

---
+++ Top Interview 150/[240802] 15. 3Sum/근영.js
@@ -0,0 +1,18 @@
+var minSubArrayLen = function(target, nums) {
+    let left = 0;
+    let sum = 0;
+    let distance = Infinity;
+
+    for(let right = 0; right < nums.length; right++){
+        sum += nums[right];
+
+        while(sum >= target){
+            distance = Math.min(distance, right - left + 1);
+            sum -= nums[left];
+            left++;
+        }
+    }
+    return distance === Infinity ? 0 : distance
+};
+
+// sliding window는 처음 보는 개념이다,.,,
