f130308ed56678677f33222877b6663ef2ffa6b2
sorryisme <sorryisme@gmail.com>
2024-09-13 06:58:21 +0900

    조금만 기다려봐 Oh my love

--- Baekjoon/Backtracking/[15649] N과 M(1)/도윤.py
+++ Baekjoon/Backtracking/[15649] N과 M(1)/도윤.py
@@ -1,22 +1,26 @@
-def recursive(k):
-    if(k == M+1):
-        print(' '.join(map(str, selected[1:M+1])))
-    else: 
-        for i in range (1,N+1):
-            if not used[i]:
-                selected[k] = i
-                used[i] = True
-                recursive(k+1)
-                selected[k] = 0
-                used[i] = False
-
-        
-
-
+def dfs(n, lst):
+    
+    # 종료 조건
+    if n == M: 
+        answer.append(lst)
+        return 
+    
+    # 하부 함수 호출
+    for j in range(1, N+1):
+        if v[j] == 0: #선택되지 않은 경우 추가
+            v[j] = 1
+            dfs(n+1, lst+[j])
+            v[j] = 0
 
 N,M = map(int,input().split())
-selected = [0] * (M + 1)
+answer =[]
+v = [0] * (N + 1)
 
-used = [False] * (N + 1)
+dfs(0, [])
 
-recursive(1)
+for lst in answer:
+    print(*lst)
+    
+    
+    
+# 재학습1
--- Baekjoon/Backtracking/[15650] N과 M(2)/도윤.py
+++ Baekjoon/Backtracking/[15650] N과 M(2)/도윤.py
@@ -1,21 +1,42 @@
-def recursive(k):
-    if k == M:
-        print(' '.join(map(str, selected[0:M])))
-    else: 
-        for i in range (1,N+1):
-            if not used[i] and (k == 0 or i > selected[k-1]): 
-                selected[k] = i
-                used[i] = True
-                recursive(k+1)
-                selected[k] = 0
-                used[i] = False
+def dfs(n, lst):
+    if n > N: 
+        if len(lst) == M:
+            ans.append(lst)
+        return
 
-        
+    dfs(n+1, lst+[n]) # 선택하는 경우
+    dfs(n+1, lst) # 선택하지 않는 경우
 
 
+
+N,M = map(int, input().split())
+ans = []
+dfs(1, [])
+
+for lst in ans:
+    print(*lst)
+
+
+## 다른 풀이2
+def dfs(n, start, lst):
+    
+    # 종료 조건
+    if n == M: 
+        answer.append(lst)
+        return 
+    
+    # 하부 함수 호출
+    for j in range(start, N+1):
+        dfs(n+1, j+1, lst+[j])
 
 N,M = map(int,input().split())
-selected = [0] * (M + 1)
-used = [False] * (N + 1)
-recursive(0)
+answer =[]
+v = [0] * (N + 1)
 
+dfs(0, 1, [])
+
+for lst in answer:
+    print(*lst)
+
+
+## 다시 공부 중
