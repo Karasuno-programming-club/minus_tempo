9a9ce243a8a0ea2c30d06f1221d2587bdd5e5148
root-zero-o <rootzero17@gmail.com>
2024-09-30 09:30:41 +0900

    아자아자파이팅

--- 자료구조/binary-search.py
+++ 자료구조/binary-search.py
@@ -37,3 +37,28 @@ def count_by_range(list, left_value, right_value):
     right_index = bisect_right(list, right_value)
     left_index = bisect_left(list, left_value)
     return right_index - left_index
+
+# 직접 구현해보기 - bisect 사용하지 않고 구현
+# left bound : target이 등장하는 가장 왼쪽 idx
+def left_bound(lst, target):
+    start = 0
+    end = len(lst) - 1
+    while start < end :
+        mid = (start + end) // 2
+        if lst[mid] >= target:
+            end = mid
+        else :
+            start = mid +1
+    return end
+
+# right bound : target보다 큰 수 중 가장 왼쪽 idx
+def right_bound(lst, target):
+    start = 0
+    end = len(lst) - 1
+    while start < end :
+        mid = (start + end) // 2
+        if lst[mid] >= target:
+            start = mid + 1
+        else : 
+            end = mid
+    return start
